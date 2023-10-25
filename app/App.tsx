58fc7a5414c5166ba5ed5589683a1dc835db0928
zoodaa <jkj01230@gmail.com>
2023-10-25 23:51:45 +0900

    feat: Navigator 설정

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import Signin from '@screens/Signin';
+import Terms from '@screens/Terms';
+import Signup from '@screens/Sginup';
+import {SafeAreaView, ScrollView, StatusBar, StyleSheet} from 'react-native';
+
+const Stack = createStackNavigator();
 
 function App() {
-  return <Signin />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Signin" component={Signin} />
+          <Stack.Screen name="Terms" component={Terms} />
+          <Stack.Screen name="Signup" component={Signup} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+});
 export default App;
