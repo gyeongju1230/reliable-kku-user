15b51aa6c8328b9100d820f5d366ff3873de00e3
zoodaa <jkj01230@gmail.com>
2023-10-26 15:48:42 +0900

    feat: Login 버튼 임시 이동경로 설정

--- app/components/pages/signin/signin-button/SigninButton.tsx
+++ app/components/pages/signin/signin-button/SigninButton.tsx
@@ -1,13 +1,22 @@
 import * as styles from '@components/pages/signin/signin-button/SigninButton.style';
 import {TouchableOpacity} from 'react-native';
+import {
+  NavigationProp,
+  ParamListBase,
+  useNavigation,
+} from '@react-navigation/native';
 
 const SigninButton = () => {
+  const navigation: NavigationProp<ParamListBase> = useNavigation();
+
   return (
     <TouchableOpacity>
       <styles.Box>
-        <styles.SigninBox>
-          <styles.SigninButton>LOGIN</styles.SigninButton>
-        </styles.SigninBox>
+        <TouchableOpacity onPress={() => navigation.navigate('BottomTabs')}>
+          <styles.SigninBox>
+            <styles.SigninButton>LOGIN</styles.SigninButton>
+          </styles.SigninBox>
+        </TouchableOpacity>
       </styles.Box>
     </TouchableOpacity>
   );
