1f2447ae80195442727c66e89054651d81ef1e45
zoodaa <jkj01230@gmail.com>
2023-10-29 01:33:15 +0900

    refactor: 회원가입 페이지 style 수정

--- app/components/pages/signup/signup-terms/signup-terms-layout/SignupTermsLayout.style.tsx
+++ app/components/pages/signup/signup-terms/signup-terms-layout/SignupTermsLayout.style.tsx
@@ -1,6 +1,15 @@
 import styled from 'styled-components/native';
+import {View} from 'react-native';
 
 export const Box = styled.View`
   flex: 1;
   background: #fff;
+`;
+
+export const SignupLogoTop = styled.View`
+  height: 16.4319248%;
+`;
+
+export const SignupTermsContentTop = styled.View`
+  height: 5.8685446%;
 `;
--- app/components/pages/signup/signup-terms/signup-terms-layout/SignupTermsLayout.tsx
+++ app/components/pages/signup/signup-terms/signup-terms-layout/SignupTermsLayout.tsx
@@ -1,14 +1,13 @@
 import * as styles from '@components/pages/signup/signup-terms/signup-terms-layout/SignupTermsLayout.style';
 import SignupLogo from '@components/pages/signup/signup-terms/signup-logo/SignupLogo';
 import SignupTermsContent from '@components/pages/signup/signup-terms/signup-terms-content/SignupTermsContent';
-import MarginTop from '@components/common/MarginTop';
 
 const SignupTermsLayout = () => {
   return (
     <styles.Box>
-      <MarginTop height={140} />
+      <styles.SignupLogoTop />
       <SignupLogo />
-      <MarginTop height={50} />
+      <styles.SignupTermsContentTop />
       <SignupTermsContent />
     </styles.Box>
   );
