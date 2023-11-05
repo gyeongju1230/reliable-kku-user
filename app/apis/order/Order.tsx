f8cb470831b072967aba67368048ed3fcc92d3ed
zoodaa <jkj01230@gmail.com>
2023-11-06 05:30:44 +0900

    feat: API 구현

--- app/apis/order/Order.tsx
+++ app/apis/order/Order.tsx
@@ -17,3 +17,37 @@ export const OrderMenuList = async () => {
     throw error;
   }
 };
+
+/* 가게 영업유무 */
+export const StoresOpenClosed = () =>
+  BASE_API.get(`/api/v1/stores/open-closed`, {})
+    .then(response => response.data)
+
+    .catch(error => {
+      console.log('가게 영업유무 확인 실패:', error.response.data);
+      throw error;
+    });
+
+/* 결제 승인 */
+export const PaymentConfirm = async (
+  paymentKey: string,
+  orderId: string,
+  amount: number,
+) => {
+  try {
+    const response = await BASE_API.post(
+      '/api/v1/payments/confirm',
+      {
+        paymentKey: paymentKey,
+        orderId: orderId,
+        amount: amount,
+      },
+      {},
+    ).then(response => {
+      console.log('결제승인 성공', response.data);
+      return response;
+    });
+  } catch (error) {
+    throw error;
+  }
+};
