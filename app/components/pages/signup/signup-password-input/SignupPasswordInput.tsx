d7f6da80704e9582473e4a27b75f164a6494df38
wan2daaa <wan2daaa@gmail.com>
2023-12-02 21:50:54 +0900

    fix: 비밀번호 정규식 수정

--- app/components/pages/signup/signup-password-input/SignupPasswordInput.tsx
+++ app/components/pages/signup/signup-password-input/SignupPasswordInput.tsx
@@ -13,7 +13,7 @@ interface SignupPasswordInputProps {
 
 const isPasswordValid = (password: string) => {
   const passwordRegex =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+    /^(?=.*[a-z])(?=.*\d)(?=.*[~.\\!@#$%^&*()_\-+=\[\]{}|;:‘“<>,.?/])[A-Za-z\d~.\\!@#$%^&*()_\-+=\[\]{}|;:‘“<>,.?/]{8,20}$/;
   return passwordRegex.test(password);
 };
 
